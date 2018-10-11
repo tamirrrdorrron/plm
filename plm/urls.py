aff97e1fe2f597487801a332255fbcbf007dc681
Tamir <tamir@chipchop.com.au>
2018-10-11 21:43:09 +1100

    added links to style pages

--- plm/urls.py
+++ plm/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from .views import index
+from .views import index, style
 
 
 urlpatterns = [
     path('', index, name='index'),
+    path('style/<str:style_code>', style, name='style'),
  ]
